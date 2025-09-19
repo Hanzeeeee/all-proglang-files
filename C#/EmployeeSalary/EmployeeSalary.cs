af97995fd52345cc1b7c9efc86a786f386d10a59
Christ Hanzen Rallos <144919761+Hanzeeeee@users.noreply.github.com>
2025-09-19 14:45:27 +0800

    Add files via upload

---
+++ C#/EmployeeSalary/EmployeeSalary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EmployeeSalary
+{
+    public class EmployeeSalary
+    {
+        public string Name { get; set; }
+        public int HoursWorked { get; set; }
+
+        public EmployeeSalary(string name, int hoursWorked)
+        {
+            Name = name;
+            HoursWorked = hoursWorked;
+        }
+
+        public double ComputeSalary()
+        {
+            int regularHours = 40;
+            double regularRate = 50;
+            double overtimeRate = 30;
+
+            if (HoursWorked <= regularHours)
+            {
+                return HoursWorked * regularRate;
+            }
+            else
+            {
+                int overtimeHours = HoursWorked - regularHours;
+                return (regularHours * regularRate) + (overtimeHours * overtimeRate);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Employee Name: {Name}\n" +
+                   $"Hours Worked: {HoursWorked}\n" +
+                   $"\n{Name}`s Salary is {ComputeSalary():F2}";
+        }
+        public void display()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+
+}
---
+++ C#/EmployeeSalary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeSalary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Employee Name: ");
+            string empName = Console.ReadLine();
+
+            Console.Write("Enter Hours Worked: ");
+            int hrsWorked = Convert.ToInt32(Console.ReadLine());
+
+            EmployeeSalary emp = new EmployeeSalary(empName, hrsWorked);
+
+            emp.display();
+        }
+    }
+}
